2180af6691c75d7d7f93a4168a171ca9c992224e
raagul <raghulkarthikeyansmct2019@citchennai.net>
2023-12-28 12:57:02 +0530

    TRYING ADD PATCH-1

---
+++ add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int x,y;
+	printf("Enter a number : ");
+	scanf("%d",&x);
+	printf("Enter a number : ");
+	scanf("%d",&y);
+	printf("Sum of 2 Numbers : %d\n",x+y);
+	return 0;
+}
+
+
